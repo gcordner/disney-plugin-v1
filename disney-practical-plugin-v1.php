bdfc404286f0caccaf0fe353bf0af18ae230ae12
gcordner <geoffcordner@gmail.com>
2024-10-11 18:38:47 +0000

    init commit

---
+++ disney-practical-plugin-v1.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Disney Practical Plugin V1
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     disney-practical-plugin-v1
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Disney_Practical_Plugin_V1
+ */
+
+// Your code starts here.
